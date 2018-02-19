c66b928a251822738db91258b1f3db584149482f
Charan Teja Reddy <charante@codeaurora.org>
2018-02-19 15:32:28 +0530

    iommu: arm-smmu: handle slave side secure targets with no static-cb option
    On slave side secure targets with no secure usecase, static cb option
    will not be set in dt. Handle such cases so that SMMU programming is
    done correctly by driver.
    
    Change-Id: Ib05f862d1f7345e364ee37f702e811547c83c3d1
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -3858,6 +3858,9 @@ bool arm_smmu_skip_write(void __iomem *addr)
 	if (!smmu)
 		return true;
 
+	if (!arm_smmu_is_static_cb(smmu))
+		return false;
+
 	/* Do not write to global space */
 	if (((unsigned long)addr & (smmu->numpage - 1)) < (smmu->numpage >> 1))
 		return true;
@@ -5436,6 +5439,11 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 		goto out_exit_power_resources;
 
 	smmu->sec_id = msm_dev_to_device_id(dev);
+	INIT_LIST_HEAD(&smmu->list);
+	spin_lock(&arm_smmu_devices_lock);
+	list_add(&smmu->list, &arm_smmu_devices);
+	spin_unlock(&arm_smmu_devices_lock);
+
 	err = arm_smmu_device_cfg_probe(smmu);
 	if (err)
 		goto out_power_off;
@@ -5516,11 +5524,6 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 		pm_runtime_enable(dev);
 	}
 
-	INIT_LIST_HEAD(&smmu->list);
-	spin_lock(&arm_smmu_devices_lock);
-	list_add(&smmu->list, &arm_smmu_devices);
-	spin_unlock(&arm_smmu_devices_lock);
-
 	/*
 	 * For ACPI and generic DT bindings, an SMMU will be probed before
 	 * any device which might need it, so we want the bus ops in place
@@ -5537,6 +5540,9 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 
 out_power_off:
 	arm_smmu_power_off(smmu, smmu->pwr);
+	spin_lock(&arm_smmu_devices_lock);
+	list_del(&smmu->list);
+	spin_unlock(&arm_smmu_devices_lock);
 
 out_exit_power_resources:
 	arm_smmu_exit_power_resources(smmu->pwr);
