4b017051d714b663360d132fa6c74dee32e95d92
Yumi Yukimura <me.cafebabe@gmail.com>
2024-05-30 23:07:15 +0800

    Revert "[TMP] init: goodbye"
    This reverts commit d1974b651ca545e14b204022a40b29346fc63460.

--- init/main.c
+++ init/main.c
@@ -1128,8 +1128,6 @@ static int __ref kernel_init(void *unused)
 	place_marker("M - DRIVER Kernel Boot Done");
 #endif
 
-	panic("goodbye");
-
 	if (ramdisk_execute_command) {
 		ret = run_init_process(ramdisk_execute_command);
 		if (!ret)
